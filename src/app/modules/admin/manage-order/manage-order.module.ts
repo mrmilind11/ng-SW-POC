17716fc8ee5fb38dcd584a32d787ab7b26a06748
Milind Sharma <milind.sharma@searce.com>
2020-04-02 15:06:37 +0530

    Two sample modules added in admin module

---
+++ src/app/modules/admin/manage-order/manage-order.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManageOrderRoutingModule } from './manage-order-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManageOrderRoutingModule
+  ]
+})
+export class ManageOrderModule { }
---
+++ src/app/modules/admin/manage-products/manage-products.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManageProductsRoutingModule } from './manage-products-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManageProductsRoutingModule
+  ]
+})
+export class ManageProductsModule { }
